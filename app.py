f015a3daff77144a558fbf16125cbd6953f2466a
Kev1nLU <kevinleonuma@gmail.com>
2023-03-27 19:46:45 -0600

    Here a message forthe first commit test 1..2..3 !

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Hello World!</p>"
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f'Hello  {escape(name)}, Welcome!!'
+
+@app.route('/user/<int:userid>')
+def userid(userid):    
+    return f'Hello user number#: {escape(userid)}'
