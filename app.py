4ec37b1c32d10c9ddb6ff14f3699018a1edc3e1b
Kev1nLU <kevinleonuma@gmail.com>
2023-03-31 21:01:36 -0600

    Jobs list added, ready for cloud deploy

--- app.py
+++ app.py
@@ -5,9 +5,36 @@ venv\Scripts\activate
 '''
 app = Flask(__name__)
 
+JOBS = [
+    {
+       'id':1,
+       'title': 'Data Analyst',
+       'location':'San Pedro, San José',
+       'salary': '$975'
+    },
+    {
+       'id':2,
+       'title': 'Data Analyst II',
+       'location':'San Pedro, San José',
+       'salary': '$1350'
+    },
+    {
+       'id':3,
+       'title': 'Junior Python Developer',
+       'location':'Santa Ana, Heredia',
+       'salary': '$1500'
+    },
+    {
+       'id':4,
+       'title': 'Senior Python Developer',
+       'location':'Hybrid',
+       
+    },
+]
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', jobs = JOBS, company_name = 'Leone Jobs')
 
 @app.route('/hello/<name>')
 def hello(name):
